57da3eca8bb5b88ff8ddd36c6951f95a0a88f47d
Liss McCabe <fmccabe@quickleft.com>
2015-12-06 14:17:05 -0800

    Add :min-lein to project.clj

--- project.clj
+++ project.clj
@@ -6,6 +6,7 @@
                  [org.clojure/data.json "0.2.6"]
                  [com.apa512/rethinkdb "0.11.0"]
                  [lein-dotenv "RELEASE"]]
+  :min-lein-version "2.0.0"
   :ring {:handler clj-rpbot.handler/app}
   :uberjar-name "server.jar"
   :profiles {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]]
