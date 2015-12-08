d1e7b5e71a65761d63d6406a4cc7a8899ffb93f2
Liss McCabe <fmccabe@quickleft.com>
2015-12-08 14:22:25 -0800

    Bump Clojure version for RethinkDB

--- project.clj
+++ project.clj
@@ -1,15 +1,15 @@
 (defproject clj-rpbot "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [clj-time "0.9.0"] ; required due to bug in lein-ring
                  [org.clojure/data.json "0.2.6"]
                  [metosin/compojure-api "0.22.0"]
                  [com.apa512/rethinkdb "0.11.0"]
                  [lein-dotenv "RELEASE"]]
   :min-lein-version "2.0.0"
-  :jvm-opts     ^:replace ["-Xss512k" "-Xms80m" "-Xmx100m"
-                           "-XX:MaxMetaspaceSize=100m"
-                           "-XX:MaxDirectMemorySize=40m"]
+  ;; :jvm-opts     ^:replace ["-Xss512k" "-Xms80m" "-Xmx100m"
+  ;;                          "-XX:MaxMetaspaceSize=100m"
+  ;;                          "-XX:MaxDirectMemorySize=40m"]
   :ring {:handler clj-rpbot.handler/app}
   :uberjar-name "server.jar"
   :profiles {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]]
--- src/clj_rpbot/character.clj
+++ src/clj_rpbot/character.clj
@@ -1,30 +1,66 @@
-;;(ns clj-rpbot.character
-;;  (:require [rethinkdb.query :as r]))
-(ns clj-rpbot.character)
+(ns clj-rpbot.character
+  (:require [rethinkdb.query :as r]))
 
-(def alice {:name "Alice"
-            :user "l"
-            :description "is a cute redhead"})
-(def andrea {:name "Andrea"
-             :user "a"
-             :description "is a cute pixie"})
-(def kale {:name "Kale"
-           :user "t"
-           :description "is a cute cyborg boy"})
-(def kylie {:name "Kylie"
-            :user "t"
-            :description "is a cute cyborg girl"})
+(with-open [conn (r/connect :host "127.0.0.1" :port 28015 :db "test")]
+  (r/run (r/db-create "test") conn)
 
-(def characters {"Alice" alice
-                 "Andrea" andrea
-                 "Kale" kale
-                 "Kylie" kylie})
+  (defn create-characters-table
+    (-> (r/db "test")
+        (r/table-create "characters")
+        (r/run conn)))
 
-(defn present-description [character]
-  (let [name (get character :name)
-        description (get character :description)]
-    (str name " " description)))
+  (-> (r/db "test")
+      (r/table "characters")
+      (r/index-create "user" (r/fn [row]
+                               (r/get-field row :user)))
+      (r/run conn))
 
-(defn show [name]
-  (let [character (get characters name)]
-    (present-description character)))
+  (-> (r/db "test")
+      (r/table "characters")
+      (r/insert [{:name "Alice"
+                  :user "l"
+                  :description "is a cute redhead"}
+                 {:name "Andrea"
+                  :user "a"
+                  :description "is a cute pixie"}
+                 {:name "Kale"
+                  :user "t"
+                  :description "is a cute cyborg boy"}
+                 {:name "Kylie"
+                  :user "t"
+                  :description "is a cute cyborg girl"}])
+      (r/run conn))
+
+  (-> (r/db "test")
+      (r/table "characters")
+      (r/get-all ["t"] {:index "user"})
+      (r/filter (r/fn [row]
+                  (r/eq "Kylie" (r/get-field row "name"))))
+      (r/run conn))
+  ;;
+  ;; (def alice {:name "Alice"
+  ;;             :user "l"
+  ;;             :description "is a cute redhead"})
+  ;; (def andrea {:name "Andrea"
+  ;;              :user "a"
+  ;;              :description "is a cute pixie"})
+  ;; (def kale {:name "Kale"
+  ;;            :user "t"
+  ;;            :description "is a cute cyborg boy"})
+  ;; (def kylie {:name "Kylie"
+  ;;             :user "t"
+  ;;             :description "is a cute cyborg girl"})
+  ;;
+  ;; (def characters {"Alice" alice
+  ;;                  "Andrea" andrea
+  ;;                  "Kale" kale
+  ;;                  "Kylie" kylie})
+  ;;
+  ;; (defn present-description [character]
+  ;;   (let [name (get character :name)
+  ;;         description (get character :description)]
+  ;;     (str name " " description)))
+  ;;
+  ;; (defn show [name]
+  ;;   (let [character (get characters name)]
+  ;;     (present-description character)))
